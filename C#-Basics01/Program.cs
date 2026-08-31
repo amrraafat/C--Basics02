ad8044a05d2e831cdff7ca6d328140d384e3a9e0
AmrRaafat <amrraafat>
2026-08-31 14:11:14 +0300

    Question 1: Book Object and Object Variable

--- C#-Basics01/Program.cs
+++ C#-Basics01/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region Question 01 - Book Object and Storage in Object Variable
+// Q1: Create a Book and put it in an object variable
+Book myBook = new Book { Title = "C# Basics", Pages = 300 };
+object objBook = myBook;
+Console.WriteLine(objBook);
+#endregion
+
+
+#region Book Class Definition
+// Basic Book class for the assignment
+/// <summary>
+/// Represents a book entity used across the assignment tasks.
+/// </summary>
+/// <param name="title">The title of the book.</param>
+/// <param name="pages">The number of pages in the book.</param>
+class Book
+{
+    public string? Title;
+    public int Pages;
+}
+#endregion
